2d50ced3b31d37967100aa1a59c46fa9f1f62ae1
Ivan <51539420+Cheewbacca@users.noreply.github.com>
2023-05-04 22:46:01 +0300

    mysql fix

--- server/mysql.js
+++ server/mysql.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "1927",
+  host: "79.132.138.89",
+  user: "makas2705",
+  password: "makas228322",
   database: "diplom",
 });
 
